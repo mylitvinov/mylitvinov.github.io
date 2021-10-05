907209ade5f2629b5a9ae9a703f86609be7ff46a
mylitvinov <vinchester30@mail.ru>
2021-10-05 22:29:07 +0300

    edit projects

--- 01-cards-sources/app.js
+++ 01-cards-sources/app.js
@@ -1,14 +1,26 @@
-const slides = document.querySelectorAll('.slide')
+function slidesPlagin(activeSlide) {
+  const slides = document.querySelectorAll(".slide");
+  clearActiveClasses();
+  slides[activeSlide].classList.add("active");
 
-for (const slide of slides) {
-  slide.addEventListener('click', ()=>{
-    clearActiveClasses();
-    slide.classList.add('active')
-  })
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+      clearInterval(setSlidePlagin);
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
 }
 
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    slide.classList.remove('active')
-  })
-}
+const getRandomNumber = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const setSlidePlagin = setInterval(() => {
+  slidesPlagin(getRandomNumber(0, 4));
+}, 5000);
